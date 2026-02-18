08cda1b15d6f90630cf7fa65afce765d8cb7542c
Anurag Sharma <162544675+anuragsharma682@users.noreply.github.com>
2026-02-18 21:46:27 +0530

    Time: 10 ms (13.18%) | Memory: 46.7 MB (10.30%) - LeetSync

---
+++ 139-word-break/word-break.java
@@ -0,0 +1,26 @@
+class Solution {
+  public boolean wordBreak(String s, List<String> wordDict) {
+    return wordBreak(s, new HashSet<>(wordDict), new HashMap<>());
+  }
+
+  private boolean wordBreak(final String s, Set<String> wordSet, Map<String, Boolean> mem) {
+    if (mem.containsKey(s))
+      return mem.get(s);
+    if (wordSet.contains(s)) {
+      mem.put(s, true);
+      return true;
+    }
+
+    for (int i = 1; i < s.length(); ++i) {
+      final String prefix = s.substring(0, i);
+      final String suffix = s.substring(i);
+      if (wordSet.contains(prefix) && wordBreak(suffix, wordSet, mem)) {
+        mem.put(s, true);
+        return true;
+      }
+    }
+
+    mem.put(s, false);
+    return false;
+  }
+}
