c5618383b12906f73c10431d237f1fd31de757c8
Anurag Sharma <162544675+anuragsharma682@users.noreply.github.com>
2026-05-25 11:16:10 +0530

    Time: 27 ms (12.12%), Space: 47.3 MB (5.23%) - LeetHub

---
+++ 1340-jump-game-v/1340-jump-game-v.java
@@ -0,0 +1,24 @@
+class Solution {
+  public int maxJumps(int[] arr, int d) {
+    final int n = arr.length;
+    int[] dp = new int[n];
+    Deque<Integer> stack = new ArrayDeque<>();
+
+    for (int i = 0; i <= n; ++i) {
+      while (!stack.isEmpty() && (i == n || arr[stack.peek()] < arr[i])) {
+        List<Integer> indices = new ArrayList<>(List.of(stack.pop()));
+        while (!stack.isEmpty() && arr[stack.peek()] == arr[indices.get(0)])
+          indices.add(stack.pop());
+        for (final int j : indices) {
+          if (i < n && i - j <= d)
+            dp[i] = Math.max(dp[i], dp[j] + 1);
+          if (!stack.isEmpty() && j - stack.peek() <= d)
+            dp[stack.peek()] = Math.max(dp[stack.peek()], dp[j] + 1);
+        }
+      }
+      stack.push(i);
+    }
+
+    return Arrays.stream(dp).max().getAsInt() + 1;
+  }
+}
